4c5f6f19977c93520bd764dfd6f32908343cacea
Tolu Kalejaiye <mrkalejaiye@gmail.com>
2018-02-16 15:52:12 +0100

    changed port variable

--- server.js
+++ server.js
@@ -225,5 +225,5 @@ app.post("/checkout", function(req, res, next) {
 });
 
 app.listen(port, () => {
-  console.log("Server running on port 5000");
+  console.log(`Server running on port ${port}`);
 });
